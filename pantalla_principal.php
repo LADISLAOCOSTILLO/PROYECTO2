24ac4e36edf4499f8f3321d604c083da6791f0c2
lacoca <032287d@utea.edu.pe>
2023-04-22 07:03:11 -0500

    pantall_PRINCIPAL

--- pantalla_principal.php
+++ pantalla_principal.php
@@ -5,8 +5,10 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PANTALLA PRINCIPAL</title>
+    <link href="pantalla_principal">
+
 </head>
 <body>
-    
+<a href="*button*"></a> 
 </body>
 </html>
