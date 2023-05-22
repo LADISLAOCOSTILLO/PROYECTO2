b6e3b23b5e8dc0f5630e23b9ebb41abe39fded8c
lacoca <032287d@utea.edu.pe>
2023-05-22 09:59:17 -0500

    agregando a index

---
+++ apuntes.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        * con git checkout nombre  --> es para saltar a una de las ramas creadas
+        <br>
+        * con git branch --> es para ver que ramas y en que rama estamos
+        <br>
+        * con git branch  mas nombre --> crea una rama
+        <br>
+        * con git log --aoneline--> sirve para explorar 
+        * con git merge nombre rama--> lo que hace unir las ramas git 
+    </p>
+
+</body>
+</html>
